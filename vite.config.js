2e632438d3fc766d07a6f16bafc3764cf04c881f
NOURRY-Erwan <servicejeunesse.carantec@gmail.com>
2025-11-13 12:00:24 +0100

    feat: Configure project for GitHub Pages deployment
    Adds necessary scripts and configuration to enable deployment of the Anim'emploi application to GitHub Pages. This includes setting the `homepage` and `base` URL in `package.json` and `vite.config.js` respectively, and adding `predeploy` and `deploy` scripts utilizing `gh-pages`.

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// https://vitejs.dev/config/
+export default {
+  // L'URL de votre site sera : https://NOURRY-Erwan.github.io/Anim-emploi/
+  // La base doit donc correspondre au nom de votre dépôt.
+  base: "/Anim-emploi/", 
+};
